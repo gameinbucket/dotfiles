9b472cc5812ccc6bb45c1df6dad3f36290051312
nmm-linux <gameinbucket@github.com>
2020-04-27 20:16:05 -0400

    install list

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-echo "$(basename "$0")"
+#!/bin/bash
 
 mkdir -p ~/.fonts
 mkdir -p ~/.vim/colors
---
+++ ubuntu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# steam
+# vscode
+# wine
+# retro arch
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vlc
+sudo apt-get install mpv
+
+sudo apt-get install zsh
+sudo apt-get install vim
+sudo apt-get install nvim
+
+sudo apt-get install gparted
+sudo apt-get install dconf-editor
+
+sudo apt-get install gimp
+sudo apt-get install audacity
+
+sudo apt-get install git
+sudo apt-get install python3
+
+sudo apt-get install libpng
+sudo apt-get install libzip
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-mixer-dev
+
+sudo apt-get install virtualbox
+
+# sudo apt-get remove
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
