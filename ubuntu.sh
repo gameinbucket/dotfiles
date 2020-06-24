7b4f3c31dd7085a9830a36c8eb7b7db9601db452
nmm-home <nmm-xub>
2020-06-23 21:26:51 -0400

    reorg

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# steam
 # vscode
-# wine
-# retro arch
+# retroarch
 # openra
 
 sudo apt-get update
@@ -12,24 +10,18 @@ sudo apt-get upgrade
 sudo apt-get install redshift
 sudo apt-get install plank
 
-sudo apt-get install vlc
-# sudo apt-get install mpv
-
-sudo apt-get install zsh
-sudo apt-get install vim
-sudo apt-get install tmux
-sudo apt-get install ranger
-# sudo apt-get install nvim
-
 sudo apt-get install gparted
-# sudo apt-get install dconf-editor
-
-sudo apt-get install krita
-# sudo apt-get install audacity
-# sudo apt-get install milkytracker
 
 sudo apt-get install git
+sudo apt-get install zsh
+sudo apt-get install vim
+sudo apt-get install curl
+sudo apt-get install tmux
+sudo apt-get install ranger
 sudo apt-get install python3
+
+sudo apt-get install vlc
+sudo apt-get install steam
 
 sudo apt-get install libpng
 sudo apt-get install libzip
@@ -37,6 +29,13 @@ sudo apt-get install libsdl2-dev
 sudo apt-get install libsdl2-mixer-dev
 sudo apt-get install libvulkan-dev
 
+# sudo apt-get install dconf-editor
+# sudo apt-get install nvim
+# sudo apt-get install mpv
+# sudo apt-get install wine
+# sudo apt-get install krita
+# sudo apt-get install audacity
+# sudo apt-get install milkytracker
 # sudo apt-get install virtualbox
 
 sudo apt-get update
